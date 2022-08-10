9546a057644e388f84ca44c19a67f039527e670d
jasson <dmjasson@gmailco>
2022-08-10 00:01:09 +0000

    borrador

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+ const app = express();
+ const PORT = 3000;
+ app.get('/', function(req, res) {
+ 	res.json({"hello": "express with mongo 3"});
+ });
+ app.listen(PORT, function(){
+   console.log('Your node js server is running on PORT:',PORT);
+ });
+
